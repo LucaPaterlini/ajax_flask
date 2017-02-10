708c6f46acba8b0728509e7056e4256acc66636d
LucaPaterlini <LucaPaterlini@github.com>
2017-02-10 18:34:07 +0100

    chebellagiornata

--- test_transitions.py
+++ test_transitions.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-@app.route("/cane", methods=['GET', 'POST'])
+CORS(app)
+
+@app.route("/")
 def helloWorld():
   return "Hello, cross-origin-world!"
-if __name__ == '__main__':
-     app.run()
